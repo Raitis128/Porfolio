e950d9f6678383c1e44e242ddfdabb5c9a3e889a
Raitis <raitisiziks@gmail.com>
2024-08-23 19:50:45 +0300

    Add animations for icons, add hovers for links and buttons

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ const projects = document.getElementById("projects");
 const contact = document.getElementById("contact");
 const footer = document.getElementById("footer");
 
+//  ICONS
+const instagramElements = document.querySelectorAll(".instagram");
+const linkedinElements = document.querySelectorAll(".linkedin");
+const githubElements = document.querySelectorAll(".github");
+const linkElements = document.querySelectorAll(".link");
+
 // Certification images
 const ffcLogo = document.querySelector(".ffc-logo");
 const metaLogo = document.querySelector(
@@ -43,4 +49,52 @@ toggle.addEventListener("click", () => {
   projects.classList.toggle("light-mode-secondary-background");
   contact.classList.toggle("light-mode-primary-background");
   footer.classList.toggle("light-mode-secondary-background");
+});
+
+toggle.addEventListener("mouseover", () => {
+  toggle.classList.add("fa-beat");
+});
+
+toggle.addEventListener("mouseout", () => {
+  toggle.classList.remove("fa-beat");
+});
+
+instagramElements.forEach((instagram) => {
+  instagram.addEventListener("mouseover", () => {
+    instagram.classList.add("fa-bounce");
+  });
+
+  instagram.addEventListener("mouseout", () => {
+    instagram.classList.remove("fa-bounce");
+  });
+});
+
+linkedinElements.forEach((linkedin) => {
+  linkedin.addEventListener("mouseover", () => {
+    linkedin.classList.add("fa-bounce");
+  });
+
+  linkedin.addEventListener("mouseout", () => {
+    linkedin.classList.remove("fa-bounce");
+  });
+});
+
+githubElements.forEach((github) => {
+  github.addEventListener("mouseover", () => {
+    github.classList.add("fa-bounce");
+  });
+
+  github.addEventListener("mouseout", () => {
+    github.classList.remove("fa-bounce");
+  });
+});
+
+linkElements.forEach((link) => {
+  link.addEventListener("mouseover", () => {
+    link.classList.add("fa-bounce");
+  });
+
+  link.addEventListener("mouseout", () => {
+    link.classList.remove("fa-bounce");
+  });
 });
