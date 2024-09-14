ebafb13c268fd20e0ef8ed5fc5150e04221a6bc8
Raitis <raitisiziks@gmail.com>
2024-09-14 17:02:24 +0300

    Remove fa moon and sun icons and add toggle

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const toggle = document.getElementById("toggle");
+//! SECTIONS
 const burger = document.getElementById("burger");
 const navigation = document.getElementById("navigation");
 const header = document.getElementById("header");
@@ -11,23 +11,16 @@ const footer = document.getElementById("footer");
 const navigationHeader = document.getElementById("navigationHeader");
 const hiddenNav = document.querySelector(".hidden-nav");
 
-//  ICONS
+//! ICONS
 const burgerIcon = document.getElementById("burgerIcon");
-const moon = document.getElementById("moon");
 const instagramElements = document.querySelectorAll(".instagram");
 const linkedinElements = document.querySelectorAll(".linkedin");
 const githubElements = document.querySelectorAll(".github");
 const linkElements = document.querySelectorAll(".link");
 
-toggle.addEventListener("click", () => {
-  if (moon.classList.contains("fa-moon")) {
-    moon.classList.remove("fa-moon");
-    moon.classList.add("fa-sun");
-  } else {
-    moon.classList.remove("fa-sun");
-    moon.classList.add("fa-moon");
-  }
+//! LOCAL STORAGE
 
+const updateTheme = () => {
   navigation.classList.toggle("light-mode-primary-background");
   header.classList.toggle("light-mode-secondary-background");
   about.classList.toggle("light-mode-primary-background");
@@ -36,15 +29,25 @@ toggle.addEventListener("click", () => {
   contact.classList.toggle("light-mode-secondary-background");
   footer.classList.toggle("light-mode-primary-background");
   navigationHeader.classList.toggle("light-mode-primary-background");
-});
+};
 
-toggle.addEventListener("mouseover", () => {
-  toggle.classList.add("fa-beat");
-});
+const updateLocalStorage = () => {};
 
-toggle.addEventListener("mouseout", () => {
-  toggle.classList.remove("fa-beat");
-});
+//!  TOGGLE EVENT LISTENERS
+// toggle.addEventListener("click", () => {
+//   updateTheme();
+//   updateLocalStorage();
+// });
+
+// toggle.addEventListener("mouseover", () => {
+//   toggle.classList.add("fa-beat");
+// });
+
+// toggle.addEventListener("mouseout", () => {
+//   toggle.classList.remove("fa-beat");
+// });
+
+//!  BURGER EVENT LISTENER
 
 burger.addEventListener("click", () => {
   if (burgerIcon.classList.contains("fa-bars")) {
@@ -63,6 +66,8 @@ burger.addEventListener("click", () => {
     navigationHeader.style.position = "static";
   }
 });
+
+//! INSTAGRAM, LINKEDIN, GITHUB, LINK EVENT LISTENERS
 
 instagramElements.forEach((instagram) => {
   instagram.addEventListener("mouseover", () => {
