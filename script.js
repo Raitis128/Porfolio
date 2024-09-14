e45fa86299a50454404b5a932f935b4b0093beef
Raitis <raitisiziks@gmail.com>
2024-09-14 16:16:46 +0300

    Remove certifications section from porfolio, fix styling bugs

--- script.js
+++ script.js
@@ -19,40 +19,22 @@ const linkedinElements = document.querySelectorAll(".linkedin");
 const githubElements = document.querySelectorAll(".github");
 const linkElements = document.querySelectorAll(".link");
 
-// Certification images
-const ffcLogo = document.querySelector(".ffc-logo");
-const metaLogo = document.querySelector(
-  ".certifications-logo[src='img/icons/meta-dark.svg']"
-);
-const codecademyLogo = document.querySelector(
-  ".certifications-logo[src='img/icons/codecademy-dark.svg']"
-);
-
 toggle.addEventListener("click", () => {
   if (moon.classList.contains("fa-moon")) {
     moon.classList.remove("fa-moon");
     moon.classList.add("fa-sun");
-
-    ffcLogo.src = "img/icons/ffc-light.svg";
-    metaLogo.src = "img/icons/meta-light.svg";
-    codecademyLogo.src = "img/icons/codecademy-light.svg";
   } else {
     moon.classList.remove("fa-sun");
     moon.classList.add("fa-moon");
-
-    ffcLogo.src = "img/icons/ffc-dark.svg";
-    metaLogo.src = "img/icons/meta-dark.svg";
-    codecademyLogo.src = "img/icons/codecademy-dark.svg";
   }
 
   navigation.classList.toggle("light-mode-primary-background");
   header.classList.toggle("light-mode-secondary-background");
   about.classList.toggle("light-mode-primary-background");
   skills.classList.toggle("light-mode-secondary-background");
-  certifications.classList.toggle("light-mode-primary-background");
-  projects.classList.toggle("light-mode-secondary-background");
-  contact.classList.toggle("light-mode-primary-background");
-  footer.classList.toggle("light-mode-secondary-background");
+  projects.classList.toggle("light-mode-primary-background");
+  contact.classList.toggle("light-mode-secondary-background");
+  footer.classList.toggle("light-mode-primary-background");
   navigationHeader.classList.toggle("light-mode-primary-background");
 });
 
