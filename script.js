40de39d280a89fe4790121c565c0d7e969cf03ec
Raitis <raitisiziks@gmail.com>
2024-09-14 17:28:24 +0300

    Add logic for toggle button, save theme in localstorage, clean some styles

--- script.js
+++ script.js
@@ -1,75 +1,84 @@
 //! SECTIONS
-const burger = document.getElementById("burger");
-const navigation = document.getElementById("navigation");
-const header = document.getElementById("header");
-const about = document.getElementById("about");
-const skills = document.getElementById("skills");
-const certifications = document.getElementById("certifications");
-const projects = document.getElementById("projects");
-const contact = document.getElementById("contact");
-const footer = document.getElementById("footer");
-const navigationHeader = document.getElementById("navigationHeader");
-const hiddenNav = document.querySelector(".hidden-nav");
+const inputEl = document.getElementById("dark-mode");
+const burgerEl = document.getElementById("burger");
+const navigationEl = document.getElementById("navigation");
+const headerEl = document.getElementById("header");
+const aboutEl = document.getElementById("about");
+const skillsEl = document.getElementById("skills");
+const projectsEl = document.getElementById("projects");
+const contactEl = document.getElementById("contact");
+const footerEl = document.getElementById("footer");
+const navigationHeaderEl = document.getElementById("navigationHeader");
+const hiddenNavEl = document.querySelector(".hidden-nav");
 
 //! ICONS
 const burgerIcon = document.getElementById("burgerIcon");
-const instagramElements = document.querySelectorAll(".instagram");
-const linkedinElements = document.querySelectorAll(".linkedin");
-const githubElements = document.querySelectorAll(".github");
-const linkElements = document.querySelectorAll(".link");
+const instagramIcon = document.querySelectorAll(".instagram");
+const linkedinIcon = document.querySelectorAll(".linkedin");
+const githubIcon = document.querySelectorAll(".github");
+const linkIcon = document.querySelectorAll(".link");
 
 //! LOCAL STORAGE
 
 const updateTheme = () => {
-  navigation.classList.toggle("light-mode-primary-background");
-  header.classList.toggle("light-mode-secondary-background");
-  about.classList.toggle("light-mode-primary-background");
-  skills.classList.toggle("light-mode-secondary-background");
-  projects.classList.toggle("light-mode-primary-background");
-  contact.classList.toggle("light-mode-secondary-background");
-  footer.classList.toggle("light-mode-primary-background");
-  navigationHeader.classList.toggle("light-mode-primary-background");
+  if (inputEl.checked) {
+    navigationEl.classList.add("light-mode-primary-background");
+    headerEl.classList.add("light-mode-secondary-background");
+    aboutEl.classList.add("light-mode-primary-background");
+    skillsEl.classList.add("light-mode-secondary-background");
+    projectsEl.classList.add("light-mode-primary-background");
+    contactEl.classList.add("light-mode-secondary-background");
+    footerEl.classList.add("light-mode-primary-background");
+    navigationHeaderEl.classList.add("light-mode-primary-background");
+  } else {
+    navigationEl.classList.remove("light-mode-primary-background");
+    headerEl.classList.remove("light-mode-secondary-background");
+    aboutEl.classList.remove("light-mode-primary-background");
+    skillsEl.classList.remove("light-mode-secondary-background");
+    projectsEl.classList.remove("light-mode-primary-background");
+    contactEl.classList.remove("light-mode-secondary-background");
+    footerEl.classList.remove("light-mode-primary-background");
+    navigationHeaderEl.classList.remove("light-mode-primary-background");
+  }
 };
 
-const updateLocalStorage = () => {};
+const updateLocalStorage = () => {
+  localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+};
+
+inputEl.checked = JSON.parse(localStorage.getItem("mode")) || false;
+updateTheme();
 
 //!  TOGGLE EVENT LISTENERS
-// toggle.addEventListener("click", () => {
-//   updateTheme();
-//   updateLocalStorage();
-// });
 
-// toggle.addEventListener("mouseover", () => {
-//   toggle.classList.add("fa-beat");
-// });
-
-// toggle.addEventListener("mouseout", () => {
-//   toggle.classList.remove("fa-beat");
-// });
+inputEl.addEventListener("input", () => {
+  updateTheme();
+  updateLocalStorage();
+});
 
 //!  BURGER EVENT LISTENER
 
-burger.addEventListener("click", () => {
+burgerEl.addEventListener("click", () => {
   if (burgerIcon.classList.contains("fa-bars")) {
     burgerIcon.classList.remove("fa-bars");
     burgerIcon.classList.add("fa-xmark");
 
-    hiddenNav.style.display = "block";
-    navigation.classList.add("border");
-    navigationHeader.style.position = "fixed";
+    hiddenNavEl.style.display = "block";
+    navigationEl.classList.add("border");
+    navigationHeaderEl.style.position = "fixed";
   } else {
     burgerIcon.classList.remove("fa-xmark");
     burgerIcon.classList.add("fa-bars");
 
-    hiddenNav.style.display = "none";
-    navigation.classList.remove("border");
-    navigationHeader.style.position = "static";
+    hiddenNavEl.style.display = "none";
+    navigationEl.classList.remove("border");
+    navigationHeaderEl.style.position = "static";
   }
 });
 
 //! INSTAGRAM, LINKEDIN, GITHUB, LINK EVENT LISTENERS
 
-instagramElements.forEach((instagram) => {
+instagramIcon.forEach((instagram) => {
   instagram.addEventListener("mouseover", () => {
     instagram.classList.add("fa-bounce");
   });
@@ -79,7 +88,7 @@ instagramElements.forEach((instagram) => {
   });
 });
 
-linkedinElements.forEach((linkedin) => {
+linkedinIcon.forEach((linkedin) => {
   linkedin.addEventListener("mouseover", () => {
     linkedin.classList.add("fa-bounce");
   });
@@ -89,7 +98,7 @@ linkedinElements.forEach((linkedin) => {
   });
 });
 
-githubElements.forEach((github) => {
+githubIcon.forEach((github) => {
   github.addEventListener("mouseover", () => {
     github.classList.add("fa-bounce");
   });
@@ -99,7 +108,7 @@ githubElements.forEach((github) => {
   });
 });
 
-linkElements.forEach((link) => {
+linkIcon.forEach((link) => {
   link.addEventListener("mouseover", () => {
     link.classList.add("fa-bounce");
   });
