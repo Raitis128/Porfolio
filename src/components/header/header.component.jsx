dcfe645c331edb57dabacfc083478ad764408334
Raitis <raitisiziks@gmail.com>
2024-09-19 12:00:31 +0300

    Add auto text effect animation in header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,8 +5,53 @@ import PhoneImage from "../../assets/img/Raitis-phone.png";
 import TabletImage from "../../assets/img/Raitis-tablet.png";
 
 import Icons from "../icons/icons.component";
+import { useEffect, useRef } from "react";
 
 const Header = () => {
+  const careerIndex = useRef(0);
+  const characterIndex = useRef(0);
+
+  useEffect(() => {
+    const careers = [
+      "Web Developer",
+      "Fullstack Developer",
+      "Js Developer",
+      ".NET Developer",
+    ];
+
+    const careersEl = document.getElementById("careers");
+
+    let timeoutId;
+
+    const updateText = () => {
+      if (careersEl) {
+        careersEl.innerText = `I am a ${careers[careerIndex.current].slice(
+          0,
+          characterIndex.current
+        )}`;
+      }
+
+      characterIndex.current++;
+
+      if (characterIndex.current === careers[careerIndex.current].length + 1) {
+        careerIndex.current++;
+        characterIndex.current = 0;
+      }
+
+      if (careerIndex.current === careers.length) {
+        careerIndex.current = 0;
+      }
+
+      timeoutId = setTimeout(updateText, 150);
+    };
+
+    updateText();
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
   return (
     <HeaderComponent id="header">
       <Container>
